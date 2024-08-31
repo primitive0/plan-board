230f1c70eeda2ee52ed26fd1718f8e8bb9d64f58
primitive0 <94762195+primitive0@users.noreply.github.com>
2024-08-31 13:55:50 +0300

    Install prettier & format code using it

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import logo from './logo.svg';
 import styles from './App.module.css';
 
 function App() {
-  return (
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-};
+  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
+}
 
 export default App;
